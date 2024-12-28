33352361267c40437cd190e98dea6c32c61ff245
Nagendra <nagendra.u208@gmail.com>
2024-12-28 07:25:35 +0530

    react portfolio modified

--- react-portfolio/src/Components/Hero.jsx
+++ react-portfolio/src/Components/Hero.jsx
@@ -1,12 +1,29 @@
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import profile from "../assets/profile.jpg"
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Hero.css";
 import resume from "../assets/Nagendra_Resume.pdf";
 import AOS from 'aos';
 import '../../node_modules/aos/dist/aos.css'
 
 function Hero(){
+    const titleArray = ["Hello, I'm Nagendra 👋", "Web Developer", "UI/UX Designer", "Python Developer","Data Analyst"]
+    const [titleIndex, setTitleIndex] = useState(0)
+    const [isAnimating, setIsAnimating] = useState(false)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIsAnimating(true); // Start animation
+            setTimeout(() => {
+                setTitleIndex((prevIndex) => (prevIndex + 1) % titleArray.length);
+                setIsAnimating(false); // Reset animation
+            }, 500); // Match the animation duration
+        }, 5000);
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, [titleArray.length]);
+
+
     useEffect(()=>{
         AOS.init({
             duration: 2000, // Animation duration in milliseconds
@@ -15,9 +32,11 @@ function Hero(){
           });
     },[])
     return (
-        <section className="hero-section">
+        <section className="hero-section" id="hero">
             <div className="content">
-                <h2 className="h2">Hello, I'm Nagendra <span>👋</span></h2>
+                <h2 className={`dynamic-title ${isAnimating}?"fade-out":"fade-in"} `}>
+                    {titleArray[titleIndex]}
+                </h2>
                 <p className="description text-light">
                 &nbsp;&nbsp; &nbsp;&nbsp;A passionate Fullstack Developer with a strong foundation in React.js and Node.js. Alongside my web development skills, 
                 I'm proficient in Excel, SQL, Python, and Power BI, making me versatile in both development 
--- react-portfolio/src/Components/Navbar.jsx
+++ react-portfolio/src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import menuIcon from '../assets/menuIcon.png';
 import closeIcon from '../assets/closeIcon.png';
 import Logo from "../assets/logo2.png";
@@ -8,9 +8,24 @@ import Logo from "../assets/logo2.png";
 
 function Navbar(){
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false)
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 80);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        // Cleanup on unmount
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
 
     return (
-        <nav className="navbar">
+        <div className={`navbar-container ${isScrolled}? "scrolled":"" `}>
+            <nav className="navbar ">
             {/* <img src="./src/assets/logo.png" alt="logo" /> */}
             <a className="nav-title" href="/">N-Reactify</a>
 
@@ -19,6 +34,7 @@ function Navbar(){
                 <img className="menuBtn" src={menuOpen ? closeIcon : menuIcon} alt="menu-button" 
                 onClick={()=> setMenuOpen(!menuOpen)} />
                 <ul className="menu-items" id={`${menuOpen && "menu-open"}`} onClick={()=> setMenuOpen(false)}>
+                    <li><a href="#hero">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experience</a></li>
                     <li><a href="#projects">Projects</a></li>
@@ -27,6 +43,7 @@ function Navbar(){
                 </ul>
             </div>
         </nav>
+        </div>
     )
 }
 
